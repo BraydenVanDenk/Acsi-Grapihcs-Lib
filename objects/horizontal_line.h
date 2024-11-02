c730111a8ca9d440dc538403563ab5357fa56b8d
BraydenVanDenk <braydenvandenk123@gmail.com>
2024-11-01 20:11:47 -0400

    Update horizontal_line.h

--- objects/horizontal_line.h
+++ objects/horizontal_line.h
@@ -4,7 +4,7 @@
 void line_horizontal(int x, std::string charecter = "█") {
     int x_loop = 0;
     std::string txt = charecter;
-    while (x_loop < 0) {
+    while (x_loop < x) {
         txt = txt + charecter;
         x_loop++;
     }
